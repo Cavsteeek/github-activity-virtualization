f6045ef66a214c9cd55e372be9a72efb53266c08
Cavsteeek <uzochukwuikemdinachi@gmail.com>
2024-10-14 20:06:02 +0100

    first commit

---
+++ github_activity_virtualization/src/main/java/com/cavsteek/github_activity_virtualization/GithubActivityVirtualizationApplication.java
@@ -0,0 +1,13 @@
+package com.cavsteek.github_activity_virtualization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubActivityVirtualizationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubActivityVirtualizationApplication.class, args);
+	}
+
+}
---
+++ github_activity_virtualization/src/test/java/com/cavsteek/github_activity_virtualization/GithubActivityVirtualizationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cavsteek.github_activity_virtualization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubActivityVirtualizationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
