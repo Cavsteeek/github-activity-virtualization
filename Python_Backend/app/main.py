225f72bcb1d98a65416f199fc8186384d0ad36c4
Cavsteeek <uzochukwuikemdinachi@gmail.com>
2024-10-18 22:43:51 +0100

    feat"added my endpoints"

--- Python_Backend/app/main.py
+++ Python_Backend/app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, HTTPException
-
+from services import fetch_commits, fetch_contributors, fetch_issues
+from pydantic import BaseModel
 
 gapp = FastAPI()
 
@@ -7,3 +8,21 @@ gapp = FastAPI()
 @gapp.get("/wel")
 def root():
     return {"message": "My first project ykkk"}
+
+
+# Endpoint to get commit data
+@gapp.get("/commits")
+async def get_commits(owner: str, repo: str):
+    return await fetch_commits(owner, repo)
+
+
+# Endpoint to get contributor data
+@gapp.get("/contributors")
+async def get_contributors(owner: str, repo: str):
+    return await fetch_contributors(owner, repo)
+
+
+# Endpoint to get issues and pull request data
+@gapp.get("/issues")
+async def get_issues(owner: str, repo: str):
+    return await fetch_issues(owner, repo)
